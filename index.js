d4cfc90d7dda4cad7d51b5f57d10f9f87bea3142
kumar gaurav singh <you@example.com>
2024-02-25 10:37:23 +0530

    github push

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 
+const exampleData={"menu": 
+{  
+  "id": "file",  
+  "value": "File",  
+
+  "popup": {  
+    "menuitem": [  
+      {"value": "New", "onclick": "CreateDoc()"},  
+      {"value": "Open", "onclick": "OpenDoc()"},  
+      {"value": "Save", "onclick": "SaveDoc()"}  
+    ]  
+  }  
+}}  
+
+app.get('/github',(req,res)=>{
+  res.send(exampleData.menu.id)
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
